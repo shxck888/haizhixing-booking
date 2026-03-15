d38a333db275c50dece1a085fdf2b64d9939c5e0
shxck888 <76200973+shxck888@users.noreply.github.com>
2026-03-15 19:11:28 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyMlrst872AiC2md81wC4N-xOnQAmWtqzW4E2LgS71uBN2KMF2taX3RUpqDr9iQ_CZp/exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzCi3fElQzxMl9iP8yxpn0ab2rYw3EwI7FUJU6SPd9QEglSPEnquvY_yNG9d4Htnl0D/exec"
 };
